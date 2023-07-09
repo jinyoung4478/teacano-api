169db35ec98004bfab40aa644510980945f68222
김진영 <skswls0@daum.net>
2023-07-09 18:00:07 +0900

    Post, Delete, Patch 테스트 메서드 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -10,5 +10,20 @@ export class MoviesController {
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
   }
 }
